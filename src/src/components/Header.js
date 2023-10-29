0a275276d7aee843cd72831eca36b04f7128d81e
Hugo <hugocesar@outlook.com>
2023-10-29 16:10:34 -0300

    Ajuste menu com novas funcoes

--- src/src/components/Header.js
+++ src/src/components/Header.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import { Appbar} from 'react-native-paper';
-import {StyleSheet} from 'react-native';
+import { Appbar } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
 
-
-
-const Header = ({title, goBack, children}) =>{
+const Header = ({ title, goBack, children }) => {
+  const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical';
   return (
     <Appbar.Header style={styles.container}>
-    {
-      goBack &&
-      <Appbar.BackAction onPress={goBack} />
-    } 
-        <Appbar.Content title={title} />
-        {children}
-        </Appbar.Header>);
+      {
+        goBack &&
+        <Appbar.BackAction onPress={goBack} />
+      }
+      <Appbar.Content style={styles.content} title={title} />
+      {children}
+      <Appbar.Action icon={MORE_ICON} onPress={() => { }} />
+    </Appbar.Header>);
 };
 
 const styles = StyleSheet.create({
-  container:{
-    backgroundColor: '#BDC3CD'
+  container: {
+    backgroundColor: '#FFF',
+    marginTop: 0,
+    height: 70
   },
 });
 
--- src/src/pages/Home.js
+++ src/src/pages/Home.js
@@ -1,63 +1,89 @@
-import React, {useState} from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Button, Searchbar } from 'react-native-paper';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { Avatar, List } from 'react-native-paper';
 
 import Container from '../components/Container';
 import Header from '../components/Header';
 import Body from '../components/Body';
 
-const Projeto = () => {
-  const [searchQuery, setSearchQuery] = useState('');
+function ProjetoScreen() {
+  return (
+    <View>
+      <List.Item
+        title="Projeto Hora do almoço"
+        description="Empresa"
+        left={props => <List.Icon {...props} icon="folder" />}
+      />
+      <List.Item
+        title="To Do"
+        description="Tarefas da casa"
+        left={props => <List.Icon {...props} icon="folder" />}
+      />
+      <List.Item
+        title="Making money"
+        description="Financeiro"
+        left={props => <List.Icon {...props} icon="folder" />}
+      />
+    </View>
+  );
+}
 
-  const onChangeSearch = (query) => setSearchQuery(query);
+function EmAndamentoScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Em andamento</Text>
+    </View>
+  );
+}
+
+function FinalizadosScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Finalizados</Text>
+    </View>
+  );
+}
+
+const Tab = createMaterialTopTabNavigator();
+
+const Projeto = () => {
+
   return (
     <Container>
       <Header title="TaskBook" />
-      <Header title={'Olá, '} />
-
       <Body>
-        <Searchbar
-          placeholder="Procurar"
-          onChangeText={onChangeSearch}
-          value={searchQuery}
-        />
-        <View style={styles.homeButtons}>
-          <Button
-            style={styles.botao}
-            icon="plus"
-            mode="contained"
-            onPress={() => console.log('Pressed')}>
-            Criar projeto
-          </Button>
-          <Button
-            style={styles.botao}
-            icon="file-download"
-            mode="contained"
-            onPress={() => console.log('Pressed')}>
-            Gerar relatório
-          </Button>
-          <Button
-            style={styles.botao}
-            icon="account-group"
-            mode="contained"
-            onPress={() => console.log('Pressed')}>
-            Gerir colaboradores
-          </Button>
+        <View style={styles.usuario}>
+          <Avatar.Text size={48} label="H" />
+          <Text style={styles.usuarioTxt}> Hugo Ferreira</Text>
         </View>
       </Body>
+
+      <NavigationContainer independent={true}>
+        <Tab.Navigator>
+          <Tab.Screen name="Projetos" component={ProjetoScreen} />
+          <Tab.Screen name="Em Andamento" component={EmAndamentoScreen} />
+          <Tab.Screen name="Finalizados" component={FinalizadosScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </Container>
   );
 };
 
 const styles = StyleSheet.create({
-  homeButtons: {
-    flex: 1,
+  usuario: {
+    backgroundColor: '#FFF',
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    borderRadius: 8
   },
-  botao: {
-    marginBottom:8,
-    marginTop:18,
-    backgroundColor: '#124698',
-  },
+  usuarioTxt: {
+    paddingLeft: 16,
+    fontSize: 18
+  }
 });
 
 export default Projeto;
